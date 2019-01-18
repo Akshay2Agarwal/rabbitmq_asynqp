166b0d7fb4c6db28c41c263ca246c7292cb28299
Akshay Agarwal <akshay.agarwal@sirionlabs.com>
2019-01-18 14:31:33 +0530

    Initial commit

---
+++ messaging.py
@@ -0,0 +1,25 @@
+from consumer import Consumer
+from producer import Producer
+from concurrent.futures import ThreadPoolExecutor
+
+
+class Messaging:
+    def __init__(self, rabbitmq_config: dict, queue_config: dict, consumer_func):
+        self.rabbitmq_config = rabbitmq_config
+        self.queue_config = queue_config
+        self.consumer_func = consumer_func
+        self.queue_consumer_locks = False
+
+    def __run_consumer(self, queue: str):
+        consumer = Consumer(self.rabbitmq_config, queue, self.consumer_func)
+        consumer()
+
+    def send_message(self, message: dict):
+        if not self.queue_consumer_locks:
+            queue_consumer_pool = ThreadPoolExecutor(max_workers=len(self.queue_config["queues"]))
+            for queue in self.queue_config["queues"]:
+                queue_consumer_pool.submit(self.__run_consumer, **{'queue': queue})
+            self.queue_consumer_locks = True
+        producer = Producer(self.rabbitmq_config, self.queue_config)
+        producer.send_msg(message)
+        return
